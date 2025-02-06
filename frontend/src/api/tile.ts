f06f2f0cf58acce5f2612c4cafa048d45a470aaa
Eric Simpson <52686452+ericssimpson@users.noreply.github.com>
2025-02-06 07:58:54 -0800

    Update tile.ts

--- frontend/src/api/tile.ts
+++ frontend/src/api/tile.ts
@@ -7,8 +7,10 @@ export function computeTileLayerURL(selectedProduct: selectedProductType) {
     
     const { product_id, date, ...paramsObject} = selectedProduct
 
-    if (paramsObject.cropmask_id === 'null'){
-        paramsObject.cropmask_id = ''
+    // If cropmask_id is 'no-mask' or the string 'null', set it to an empty string.
+    // This prevents 'no-mask' from being sent and aligns with how 'null' was previously handled.
+    if (paramsObject.cropmask_id === 'no-mask' || paramsObject.cropmask_id === 'null') {
+        paramsObject.cropmask_id = '';
     }
 
     if (product_id === undefined || date === undefined) {
